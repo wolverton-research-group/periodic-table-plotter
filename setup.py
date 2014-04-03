626f384eb0fae7ebb7dca111b948a76d43c60332
Scott Kirklin <scott.kirklin@gmail.com>
2014-04-03 09:55:38 -0500

    moved main folder to more import friendly name

--- setup.py
+++ setup.py
@@ -2,17 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='periodic-table-plotter',
-    version='0.1',
+    version='0.1c',
     author='S. Kirklin',
     author_email='scott.kirklin@gmail.com',
     packages=find_packages(),
     url='http://pypi.python.org/pypi/periodic-table-plotter',
-    license='LICENSE.txt',
-    long_description=open('README.txt').read(),
-    package_data = {'': ['*.yml', '*.txt']},
+    license='LICENSE',
+    package_data = {'': ['*.yml', '*.md']},
     install_requires=[
-        'qmpy >= 0.48',
-        'numpy',
         'matplotlib'
     ],
 )
