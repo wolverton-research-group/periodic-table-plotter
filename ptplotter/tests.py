d05d216dfcbf59e67b7861ab917e9fe208789264
Scott Kirklin <scott.kirklin@gmail.com>
2014-04-14 15:05:41 -0500

    adding unittests

--- ptplotter/tests.py
+++ ptplotter/tests.py
@@ -1,6 +1,8 @@
 from plotter import ElementDataPlotter
 import matplotlib.pylab as plt
 import random
+from StringIO import StringIO
+from unittest import TestCase
 
 def eneg(elt):
     """Electronegativity"""
@@ -18,37 +20,36 @@ def atomic_number(elt):
     """Atomic number"""
     return elt['z']
 
-def test_pettifor():
-    elts = ['H', 'Li', 'Be', 'Fe', 'Ni', 'Pd', 'Pt', 'F', 'Xe', 'O', 'S']
-    epd = ElementDataPlotter(elements=elts)
-    epd.pettifor(mass, eneg)
-    plt.savefig('pettifor.png', bbox_inches='tight')
+class ElementDataPlotterTestCase(TestCase):
+    def test_pettifor(self):
+        elts = ['H', 'Li', 'Be', 'Fe', 'Ni', 'Pd', 'Pt', 'F', 'Xe', 'O', 'S']
+        epd = ElementDataPlotter(elements=elts)
+        epd.pettifor(mass, eneg)
+        f = StringIO()
+        plt.savefig(f, bbox_inches='tight')
 
-def test_periodic_table():
-    epd = ElementDataPlotter()#elements=['Fe','Ni','Li','Bi'])
-    epd.ptable([eneg])
-    plt.savefig('uni.png', bbox_inches='tight')
+    def test_periodic_table(self):
+        f = StringIO()
+        epd = ElementDataPlotter()#elements=['Fe','Ni','Li','Bi'])
+        epd.ptable([eneg])
+        plt.savefig(f, bbox_inches='tight')
 
-    epd = ElementDataPlotter()
-    epd.ptable([eneg, mass])
-    epd.create_guide_square(x=8, y=-1.5)
-    plt.savefig('bi.png', bbox_inches='tight')
+        epd = ElementDataPlotter()
+        epd.ptable([eneg, mass])
+        epd.create_guide_square(x=8, y=-1.5)
+        plt.savefig(f, bbox_inches='tight')
 
-    epd = ElementDataPlotter()
-    epd.ptable([[eneg, mass], rat])
-    plt.savefig('tri.png', bbox_inches='tight')
+        epd = ElementDataPlotter()
+        epd.ptable([[eneg, mass], rat])
+        plt.savefig(f, bbox_inches='tight')
 
-    epd = ElementDataPlotter()
-    epd.ptable([eneg, mass, rat, atomic_number], colorbar=False)
-    plt.savefig('quad.png', bbox_inches='tight')
+        epd = ElementDataPlotter()
+        epd.ptable([eneg, mass, rat, atomic_number], colorbar=False)
+        plt.savefig(f, bbox_inches='tight')
 
-def test_grid():
-    epd = ElementDataPlotter()
-    elts = [ 'Ti', 'V', 'Sc', 'Ni', 'Co', 'Fe' ]
-    epd.make_grid(xelts=elts, yelts=elts)
-    plt.savefig('grid.png', bbox_inches='tight')
-
-if __name__ == '__main__':
-    test_pettifor()
-    test_periodic_table()
-    test_grid()
+    def test_grid(self):
+        epd = ElementDataPlotter()
+        elts = [ 'Ti', 'V', 'Sc', 'Ni', 'Co', 'Fe' ]
+        epd.make_grid(xelts=elts, yelts=elts)
+        f = StringIO()
+        plt.savefig(f, bbox_inches='tight')
