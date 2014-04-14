41f4c76f28ce9956df219e489ff1b69b5f3c9b91
Scott Kirklin <scott.kirklin@gmail.com>
2014-04-14 15:59:08 -0500

    Removed actual plotting part from tests

--- ptplotter/tests.py
+++ ptplotter/tests.py
@@ -27,31 +27,23 @@ class ElementDataPlotterTestCase(TestCase):
         elts = ['H', 'Li', 'Be', 'Fe', 'Ni', 'Pd', 'Pt', 'F', 'Xe', 'O', 'S']
         epd = ElementDataPlotter(elements=elts)
         epd.pettifor(mass, eneg)
-        f = StringIO()
-        plt.savefig(f, bbox_inches='tight')
 
     def test_periodic_table(self):
         f = StringIO()
         epd = ElementDataPlotter()#elements=['Fe','Ni','Li','Bi'])
         epd.ptable([eneg])
-        plt.savefig(f, bbox_inches='tight')
 
         epd = ElementDataPlotter()
         epd.ptable([eneg, mass])
         epd.create_guide_square(x=8, y=-1.5)
-        plt.savefig(f, bbox_inches='tight')
 
         epd = ElementDataPlotter()
         epd.ptable([[eneg, mass], rat])
-        plt.savefig(f, bbox_inches='tight')
 
         epd = ElementDataPlotter()
         epd.ptable([eneg, mass, rat, atomic_number], colorbar=False)
-        plt.savefig(f, bbox_inches='tight')
 
     def test_grid(self):
         epd = ElementDataPlotter()
         elts = [ 'Ti', 'V', 'Sc', 'Ni', 'Co', 'Fe' ]
         epd.make_grid(xelts=elts, yelts=elts)
-        f = StringIO()
-        plt.savefig(f, bbox_inches='tight')
