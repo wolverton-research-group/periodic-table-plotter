d6a6a9afcdabb506dcbcb3772e1dc3341db2a95d
Scott Kirklin <scott.kirklin@gmail.com>
2014-05-01 15:59:46 -0500

    Tweaked tests

--- ptplotter/tests.py
+++ ptplotter/tests.py
@@ -1,6 +1,6 @@
 import matplotlib
 matplotlib.use('Agg')
-from plotter import ElementDataPlotter
+import plotter
 import matplotlib.pylab as plt
 import random
 from StringIO import StringIO
@@ -25,25 +25,25 @@ def atomic_number(elt):
 class ElementDataPlotterTestCase(TestCase):
     def test_pettifor(self):
         elts = ['H', 'Li', 'Be', 'Fe', 'Ni', 'Pd', 'Pt', 'F', 'Xe', 'O', 'S']
-        epd = ElementDataPlotter(elements=elts)
+        epd = plotter.ElementDataPlotter(elements=elts)
         epd.pettifor(mass, eneg)
 
     def test_periodic_table(self):
         f = StringIO()
-        epd = ElementDataPlotter()#elements=['Fe','Ni','Li','Bi'])
+        epd = plotter.ElementDataPlotter()#elements=['Fe','Ni','Li','Bi'])
         epd.ptable([eneg])
 
-        epd = ElementDataPlotter()
+        epd = plotter.ElementDataPlotter()
         epd.ptable([eneg, mass])
         epd.create_guide_square(x=8, y=-1.5)
 
-        epd = ElementDataPlotter()
+        epd = plotter.ElementDataPlotter()
         epd.ptable([[eneg, mass], rat])
 
-        epd = ElementDataPlotter()
+        epd = plotter.ElementDataPlotter()
         epd.ptable([eneg, mass, rat, atomic_number], colorbar=False)
 
     def test_grid(self):
-        epd = ElementDataPlotter()
+        epd = plotter.ElementDataPlotter()
         elts = [ 'Ti', 'V', 'Sc', 'Ni', 'Co', 'Fe' ]
         epd.make_grid(xelts=elts, yelts=elts)
